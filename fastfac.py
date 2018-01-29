7f18091553d6daad31fa5163169e71047e139ab9
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-01-29 14:58:48 -0800

    adding rudimentary flow direction function

--- fastfac.py
+++ fastfac.py
@@ -1,16 +1,34 @@
 import gdal
-import numpy
+import numpy as np
 import os
 
-def testfunction(arg1):
+def flowDirection(dem):
     """
-    Function to test autodoc generation
+    Quick flow direction implementation with ESRI coding. Does not accurately sort out flow direction in flat areas, or
+    raster edges.
 
     Args:
-        arg1: fake argument
+        dem: Numpy array of elevation values
 
     Returns:
-        None
+        numpy array with ESRI flow directions
 
     """
-    return None
+    fdir = np.empty((dem.shape[0], dem.shape[1]))
+    fdir.fill(0)
+    gradient = np.empty((8, dem.shape[0]-2, dem.shape[1]-2), dtype = np.float)
+    code = np.empty(8, dtype=np.int)
+    for k in range(8):
+        theta = -k * np.pi / 4
+        code[k] = 2 ** k
+        j, i = np.int(1.5 * np.cos(theta)), -np.int(1.5 * np.sin(theta))
+        d = np.linalg.norm([i, j])
+        gradient[k] = (dem[1 + i: dem.shape[0] - 1 + i, 1 + j: dem.shape[1] - 1 + j] - dem[1: dem.shape[0] - 1, 1: dem.shape[1] - 1]) / d
+
+    direction = (-gradient).argmax(axis=0)
+    fdir[1:-1, 1:-1] = code.take(direction)
+
+    return fdir
+
+def flowDirectionTest(dem):
+    
